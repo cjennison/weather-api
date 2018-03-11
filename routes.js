b1e8b20a340cbcaefc8de53df85afdd242893109
Chris Jennison <cjennison92@gmail.com>
2018-03-11 11:42:35 -0400

    apply lint rules to routes

--- routes.js
+++ routes.js
@@ -1,29 +1,28 @@
-const SubscriberController = require('./controllers/subscriber')
-const WeatherEmailGenerator = require('./services/email/weather-newsletter/weather-email-generator')
-const { Subscriber } = require('./db/models')
-
+const SubscriberController = require('./controllers/subscriber');
+const WeatherEmailGenerator = require('./services/email/weather-newsletter/weather-email-generator');
+const { Subscriber } = require('./db/models');
+const _ = require('lodash');
 
 module.exports = function (server) {
-  //Allow OPTIONS
-  server.opts(/\.*/, function (req, res, next) {
+  //  Allow OPTIONS
+  server.opts(/\.*/, (req, res, next) => {
     res.send(200);
     next();
   });
-  
-  //Initialize REST Routes
-  new SubscriberController(server)
+
+  //  Initialize REST Routes
+  new SubscriberController(server);
 
   server.get('/email-tester', (req, res) => {
     Subscriber.findAll().then((subscribers) => {
-      for (const subscriber of subscribers) {
+      _.forEach(subscribers, (subscriber) => {
         const emailGenerator = new WeatherEmailGenerator(subscriber);
         emailGenerator.execute();
-      }
-    })
-    .catch((err) => {
-      console.log("Could not start email tester", err)
-    })
-    
-    res.send(200, "Queued Emails")
-  })
-}
+      });
+    }).catch((err) => {
+      console.log('Could not start email tester', err);
+    });
+
+    res.send(200, 'Queued Emails');
+  });
+};
